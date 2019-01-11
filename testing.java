69eab3db3ea4c7382364ef9d89c9205363c497bb
chofia <sofia.saenz@ymail.com>
2019-01-11 14:00:22 -0500

    checking it again

--- testing.java
+++ testing.java
@@ -5,6 +5,8 @@ public class testing {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
 		System.out.println("Did it work?");
+
+		System.out.println("Checking the push thing again");
 		//adding comments
 	}
 
