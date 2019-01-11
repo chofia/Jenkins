45b17b017dda623e0176ca463b769958a72ae969
chofia <sofia.saenz@ymail.com>
2019-01-11 10:54:18 -0500

    added a print ln

--- testing.java
+++ testing.java
@@ -3,7 +3,13 @@ public class testing {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("Hello World");
 	}
 
 }
+
+
+//once it is all pushed to github add your github repo
+//in the Build Shell with the following commands
+//javac <filename>.java
+//java <filename>
